91387bf414289a1972b5e550b644078d40c94376
Kyle Conroy <kyleconroy@gmail.com>
2017-12-05 12:18:16 -0500

    add navigation to header

--- src/header.jsx
+++ src/header.jsx
@@ -69,14 +69,15 @@ export class Header extends React.Component {
     let x3 = `${0.4 * this.state.windowWidth}px`
     let x4 = `${0.6 * this.state.windowWidth}px`
     let x5 = `${0.8 * this.state.windowWidth}px`
+    let xText = `${0.5 * this.state.windowWidth}px`
 
-    let height1 = `${(0.8-scrollPerc) * this.state.windowHeight}px`
-    let y = `-${0.4 * this.state.windowHeight + scrollPerc * this.state.windowHeight}px`
+    let scrollYSticky = Math.min(scrollPerc, 0.75)
+    let y = `${-0.6 * this.state.windowHeight - scrollYSticky * this.state.windowHeight}px`
+    let yText = `${(0.25-scrollPerc) * this.state.windowHeight}px`
+    let yLinks = `${(0.8-scrollYSticky) * this.state.windowHeight}px`
 
     let rotateDeg = Math.max(25 - scrollPerc * 2 * 25, 0)
     let translateXfactor = Math.max(1 - scrollPerc * 2, 0)
-
-    console.log(height1)
 
     let transform1 = `translateX(${-125*translateXfactor}%) rotate(${rotateDeg}deg)`
     let transform2 = `translateX(${-100*translateXfactor}%) rotate(${rotateDeg}deg)`
@@ -84,17 +85,38 @@ export class Header extends React.Component {
     let transform4 = `translateX(${-50*translateXfactor}%) rotate(${rotateDeg}deg)`
     let transform5 = `translateX(${-25*translateXfactor}%) rotate(${rotateDeg}deg)`
 
-    console.log("scrollPerc" + scrollPerc)
-
     return (
       <div>
         <svg width="100%" height="100%" style={{position: 'fixed'}}>
-          <rect x={x1} y={y} width="25%" height="120%" style={{fill:blue2, transform:transform1}} className="headerRectangle" />
-          <rect x={x2} y={y} width="25%" height="120%" style={{fill:gray2, transform:transform2}} className="headerRectangle" />
-          <rect x={x3} y={y} width="25%" height="120%" style={{fill:blue3, transform:transform3}} className="headerRectangle" />
-          <rect x={x4} y={y} width="25%" height="120%" style={{fill:gray3, transform:transform4}} className="headerRectangle" />
-          <rect x={x5} y={y} width="25%" height="120%" style={{fill:blue1, transform:transform5}} className="headerRectangle" />
+          <rect x={x1} y={y} width="25%" height="140%" style={{fill:blue2, transform:transform1}} className="headerRectangle" />
+          <rect x={x2} y={y} width="25%" height="140%" style={{fill:gray2, transform:transform2}} className="headerRectangle" />
+          <rect x={x3} y={y} width="25%" height="140%" style={{fill:blue3, transform:transform3}} className="headerRectangle" />
+          <rect x={x4} y={y} width="25%" height="140%" style={{fill:gray3, transform:transform4}} className="headerRectangle" />
+          <rect x={x5} y={y} width="25%" height="140%" style={{fill:blue1, transform:transform5}} className="headerRectangle" />
         </svg>
+
+        <div className="headerText" style={{left:xText, top:yText}}>
+          <h1>Kyle E Conroy</h1>
+          <p>Astronomy Graduate Student | Vanderbilt University</p>
+        </div>
+
+        <div className="headerLinks" style={{left:x1, top:yLinks, transform:transform1}}>
+          <NavLink to="/">Home</NavLink>
+        </div>
+        <div className="headerLinks" style={{left:x2, top:yLinks, transform:transform2}}>
+          <NavLink to="/research">Research</NavLink>
+        </div>
+        <div className="headerLinks" style={{left:x3, top:yLinks, transform:transform3}}>
+          <NavLink to="/publications">Publications</NavLink>
+        </div>
+        <div className="headerLinks" style={{left:x4, top:yLinks, transform:transform4}}>
+          <NavLink to="/products">Code &amp; Products</NavLink>
+        </div>
+        <div className="headerLinks" style={{left:x5, top:yLinks, transform:transform5}}>
+          <NavLink to="/cv">Vita</NavLink>
+        </div>
+
+
       </div>
     )
   }
